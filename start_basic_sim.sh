163dbc893db7e99a1b3616caa739b528457cf66e
aezquerro <albertoezquerro@gmail.com>
2023-01-19 22:35:17 +0000

    Update launching script

--- start_basic_sim.sh
+++ start_basic_sim.sh
@@ -1,2 +1,4 @@
-. /home/simulations/ros2_sims_ws/install/setup.bash
+BOX_BOT_SIM_PATH=/home/simulations/ros2_sims_ws/install/setup.bash
+source $BOX_BOT_SIM_PATH
+export GAZEBO_MODEL_PATH=/home/simulations/ros2_sims_ws/src/box_bot/box_bot_gazebo/models:/home/simulations/ros2_sims_ws/src:/home/simulations/ros2_sims_ws/src/box_bot
 ros2 launch box_bot_gazebo main_simple.launch.xml
