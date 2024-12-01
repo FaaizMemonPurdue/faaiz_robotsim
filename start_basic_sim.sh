eaec4595fd661a4143e1321f0d2bd0d32a5462dc
FaaizMemonPurdue <fmemon@purdue.edu>
2024-11-30 20:28:50 -0500

    mostly rob cept verbose

--- start_basic_sim.sh
+++
@@ -1,4 +0,0 @@
-BOX_BOT_SIM_PATH=/home/simulations/ros2_sims_ws/install/setup.bash
-source $BOX_BOT_SIM_PATH
-export GAZEBO_MODEL_PATH=/home/simulations/ros2_sims_ws/src/box_bot/box_bot_gazebo/models:/home/simulations/ros2_sims_ws/src:/home/simulations/ros2_sims_ws/src/box_bot
-ros2 launch box_bot_gazebo main_simple.launch.xml
